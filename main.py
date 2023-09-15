14aa84db5502865932ab1a12a8422436788d65a3
k4karole <k4karole@gmail.com>
2023-09-15 20:25:41 +0100

    clear time/text background fix

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 # What did we learn?
 # The tkinter label generation sequence -> what will be on the top
 # Not the ..label1.place(x,y), ..label2.place(x,y) sequence
+# Need CANVAS to be able to display text with clear transparent background
 
 
 from tkinter import *
@@ -33,13 +34,11 @@ def save_settings(settings_data):
     return
 
 def time_display():
-    # HOURES:MINUTES
     hours_and_mins = strftime('%H:%M')
-    hours_and_mins_label.config(text=hours_and_mins)
-    # :SECONDS
     seconds = strftime(':%S')
-    seconds_label.config(text=seconds)
-    seconds_label.after(1000, time_display)
+    canvas.itemconfig(hours_and_mins_display, text=hours_and_mins)
+    canvas.itemconfig(seconds_display, text=seconds)
+    canvas.after(1000, time_display)
 
 
 def load_music():
@@ -71,79 +70,62 @@ window_background_color = selected_skin_folder['window_background_color']
 
 # MUSIC
 music_on = settings_data['music_on']
-# if  music_on:
-    # load_music()
-    # play_music()
+
+def load_and_play():
+    if  music_on:
+        load_music()
+        play_music()
 
 # WINDOW
 window = Tk()
-window.configure(background=window_background_color) # FYI - not necessery / not working if the image background color is set
+# window.configure(background=window_background_color) # FYI - not necessery / not working if the image background color is set
 window.title(selected_skin_folder['window_title'])
-window_width = 800
-window_length = 500
+window_width = 720 + 2
+window_high = 486 + 2
 screen_width = window.winfo_screenwidth()
 screen_height = window.winfo_screenheight()
-window.geometry(f'{window_width}x{window_length}+%d+%d' % (screen_width*0.2, screen_height*0.6))
+window.geometry(f'{window_width}x{window_high}+%d+%d' % (screen_width*0.2, screen_height*0.6))
 window.resizable(0,0)   # locks the main window
 
-# BG GIF
+## BG GIF
 path_gif = Path(working_directory, "skins", skin_selected, "GIF.GIF")
 get_frames_count_all = Image.open(path_gif)
 frames_count_all = get_frames_count_all.n_frames  # gives total number of frames that gif contains
 
-# creating list of PhotoImage objects for each frames
-images_list = [PhotoImage(file=path_gif, format=f"gif -index {i}") for i in range(frames_count_all)]
+
+images_list = [PhotoImage(file=path_gif, format=f"gif -index {i}") for i in range(frames_count_all)]  # creating list of PhotoImage objects for each frames
 
 count = 0
 anim = None
-animation_speed = 50       # 1000 = 1 sec
+animation_speed = 80      # 1000 = 1 sec
 def animation(count):
     image_next = images_list[count]
-    gif_label.configure(image=image_next)
-    count += 1
-    if count == frames_count_all-1:     # -1: the last frame/image is not usable, too noise
-        count = 0
-    window.after(animation_speed, lambda :animation(count))
+    canvas.itemconfig(image_display, image=image_next, anchor=NW)
 
+    count += 1
+    if count == frames_count_all-1:     # -1: the last frame/image is not usable, too noisy
+        count = 0
+    canvas.after(animation_speed, lambda :animation(count))
 
 
 # BG IMAGE
-path_image = Path(working_directory, "skins", skin_selected, "BG.PNG")      # Path functions makes the path OS independent
-backgound_image = PhotoImage(file = path_image)
-
+# path_image = Path(working_directory, "skins", skin_selected, "BG.PNG")      # Path functions makes the path OS independent
+# backgound_image = PhotoImage(file = path_image)
 
 
 if platform.system() == 'Windows':      # will not be visible on Linux, macOS
     path_icon = Path(working_directory, "skins", skin_selected, "icon.ico")
     window.iconbitmap(path_icon)        # window icon - left, top corner
 
-# LABELS
-# IMAAGES
-backgound_image_label = Label(window, image = backgound_image, borderwidth=0)
-# GIF
-gif_label = Label(window, borderwidth=0)
-# TIME
-hours_and_mins_label = Label(window, font=('calibri', 100, 'bold'),
-            background='red',
-            foreground='black')
+canvas = Canvas(window, width=window_width, height=window_high)
+canvas.place(x=0,y=0)
 
-seconds_label = Label(window, font=('calibri', 70, 'bold'),
-            background='#420000',
-            foreground='black')
+image_display = canvas.create_image((0,0))
+hours_and_mins_display = canvas.create_text((20, window_high), text=strftime('%H:%M'), font="MSGothic 80 bold", fill="#FF423E", anchor=SW)
+seconds_display = canvas.create_text((295, window_high-10), text=strftime(':%S'), font="MSGothic 50 bold", fill="#FF423E", anchor=SW)
 
-
-## WIDGET PLACEMENT
-# IMAGE
-backgound_image_label.place(x = -2, y = 0)
-# GIF
-gif_label.place(x=0, y=0)
-# TIME
-hours_and_mins_label.place(x=50, y=50)
-seconds_label.place(x=360, y=85)
-# BUTTONS
-
-
-animation(count)
 time_display()
+animation(count)
+load_and_play()
 
 window.mainloop()
