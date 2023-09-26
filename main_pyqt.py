6c76064c367cc9b9d4e821e0b959cbfd0a210b34
k4karole <k4karole@gmail.com>
2023-09-26 19:27:08 +0100

    setStyleSheet for buttons

--- main_pyqt.py
+++ main_pyqt.py
@@ -10,7 +10,7 @@ from PyQt6.QtCore import QSize
 
 if __name__ == "__main__":
     
-    app = QApplication([])
+    app = QApplication(sys.argv)
 
     window = QMainWindow()
     window.resize(720, 486)
@@ -19,15 +19,42 @@ if __name__ == "__main__":
 
     label_animation = QLabel(window)
     
-    icon_size = QSize(25,25)
-    icon = QIcon('skins/_icons/icon_settings.png')
- 
-    button_settings = QPushButton(window, text=None, icon=icon)
-    button_settings.setIconSize(icon_size)
-    button_settings.setFlat(False)
-    button_settings.setStyleSheet("background-color: #AF0000")
-    button_settings.setGeometry(100, 100, 35, 35)
    
+    
+    # BUTTON - SETTINGS
+    icon_settings = QIcon('skins/_icons/icon_settings.png')
+    button_settings = QPushButton(window, text=None, icon=icon_settings)
+    button_settings.setIconSize(QSize(20,20))       # icon sizing
+    button_settings.setGeometry(20, 30, 30, 30)     # pos, pos, size, size
+
+
+    # BUTTON - SETTINGS
+    icon_settings = QIcon('skins/_icons/icon_start.png')
+    button_music = QPushButton(window, text=None, icon=icon_settings)
+    button_music.setIconSize(QSize(20,20))
+    button_music.setGeometry(20, 62, 29, 29)     # pos, pos, size, size
+  
+    skin_selected = 'terminator'
+
+    
+    # SKINS
+    if skin_selected == 'terminator':
+
+        app.setStyleSheet("QPushButton"
+                            # DEFAULT
+                            "{"
+                            "background-color : #AF0000;"
+                            "border-radius: 5px;"          # corner roundness
+                            "border: 2px solid black;"
+                            "}"
+                            # CLICKED
+                            "QPushButton::pressed"
+                            "{"
+                            "background-color : #400000;"
+                            "}"
+                            )
+
+
 
     
     skin_selected = 'terminator'
@@ -56,6 +83,6 @@ if __name__ == "__main__":
     # if user_input: movie.setSpeed(user_input)
 
 
-    app.exec()
+    # app.exec()
 
-    # sys.exit(app.exec()) - unnecessary?
+    sys.exit(app.exec())
