abb59d8e80113674c7e2c6cc762244343dd40db2
k4karole <k4karole@gmail.com>
2023-10-03 03:49:25 +0100

    simplified

--- docs/learning/pyqt6_image_display.py
+++ docs/learning/pyqt6_image_display.py
@@ -1,31 +1,37 @@
-import sys
-from pathlib import Path
-
-from PyQt6.QtCore import QDir
+from PyQt6.QtWidgets import QMainWindow, QApplication, QLabel
 from PyQt6.QtGui import QPixmap
-from PyQt6.QtWidgets import QApplication, QToolButton, QWidget, QVBoxLayout, QLabel, QMainWindow
-
-image_path = QDir('skins/terminator/GIF.GIF').path()
-
-class MyApp(QMainWindow):
+from PyQt6.QtCore import Qt
+import sys
+ 
+ 
+class Window(QMainWindow):
     def __init__(self):
         super().__init__()
+       
+        self.setWindowTitle("Image")
+        self.setGeometry(0, 0, 400, 300)
+ 
+        # FIRST IMAGE - SMOOTHTRANSFORMATION
+        self.label = QLabel(self)
+        self.pixmap = QPixmap('skins/_icons/icon_settings.png').scaledToWidth(30, Qt.TransformationMode.SmoothTransformation)
+        self.label.setPixmap(self.pixmap)
+        self.label.resize(self.pixmap.width(),
+                          self.pixmap.height())
+        self.label.move(70, 70)
 
-        self.setWindowTitle("Display Image")
+        # SECOND IMAGE
+        label_2 = QLabel(self)
+        pixmap_2 = QPixmap('skins/_icons/icon_settings.png').scaledToWidth(30)
+        label_2.setPixmap(pixmap_2)
+        label_2.resize(pixmap_2.width(),
+                          pixmap_2.height())
+        label_2.move(120, 70)
+ 
 
-        c_widget = QWidget()
-        layout = QVBoxLayout(c_widget)
+        self.move(800, 800)
+        self.show()
+ 
 
-        pixmap = QPixmap(image_path)
-
-        label = QLabel()
-        label.setPixmap(pixmap)
-
-        layout.addWidget(label)
-
-        self.setCentralWidget(c_widget)
-
-app = QApplication(sys.argv)
-window = MyApp()
-window.show()
-app.exec()
+App = QApplication(sys.argv)
+window = Window()
+sys.exit(App.exec())
